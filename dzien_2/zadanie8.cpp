f6280cad0ec7376742b90817f27acc2abe214959
Maciej <maciej.lasota@interia.pl>
2022-11-06 12:24:20 +0100

    dodanie dzien_2

---
+++ dzien_2/zadanie8.cpp
@@ -0,0 +1,21 @@
+// przyjmij liczbę N i wypisz wiersz złożony z N znaków #
+// dla N = 6 wypisujemy
+// ######
+
+#include <iostream>
+
+int main()
+{
+    int x = 0;
+    
+    std::cin >> x;
+    
+    while (x > 0)
+    {
+        std::cout << "#";
+        x -=1;
+    }
+    
+    std::cout << "\n";
+    
+}
